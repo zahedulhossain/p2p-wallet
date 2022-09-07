115119d2bb69f258f94e16c597b7d3f651137de4
zahedulhossain <md.zahedulhossain@gmail.com>
2022-09-07 16:26:07 +0600

    refactor: make user registration simple

--- app/Http/Controllers/Api/RegisteredUserController.php
+++ app/Http/Controllers/Api/RegisteredUserController.php
@@ -4,14 +4,23 @@ namespace App\Http\Controllers\Api;
 
 use App\Actions\CreateNewUser;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisteredUserController extends Controller
 {
-    public function __invoke(Request $request, CreateNewUser $creator): \Illuminate\Http\JsonResponse
+    public function __invoke(RegisterRequest $request, CreateNewUser $creator): \Illuminate\Http\JsonResponse
     {
-        event(new Registered($user = $creator->create($request->all())));
+        $user = User::query()->create([
+            'name' => $request->validated('name'),
+            'email' => $request->validated('email'),
+            'password' => Hash::make($request->validated('password')),
+        ]);
+
+        event(new Registered($user));
 
         return response()->json([
             'data' => [
